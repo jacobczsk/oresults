f184b9c97b3a7f67e7ea18cea1bb61132088bb9d
JacobCZ <jjiroutek6@gmail.com>
2023-09-30 08:52:12 +0200

    Updates

--- script/Config.js
+++ script/Config.js
@@ -3,7 +3,7 @@ var OResults;
 (function (OResults) {
     var Config;
     (function (Config) {
-        Config.COMP_ID = 26774;
+        Config.COMP_ID = 27819; //26774;
         Config.LOGO_PATH = "img/logo.png";
     })(Config = OResults.Config || (OResults.Config = {}));
 })(OResults || (OResults = {}));
